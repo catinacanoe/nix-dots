d633273b3013612fa07ea1d84ddf59c417dacf9f
catinacanoe <catinacanoe@proton.me>
2024-02-28 18:18:59 -0800

    fixed misbind (ctrl+h,n,a,i,o readjusted on keyboard)

--- home/remap/modules/workspaces.nix
+++ home/remap/modules/workspaces.nix
@@ -8,21 +8,44 @@ let
         ${pad}${hypr "workspace ${ws}"}
     ${pad}${mod}-shift-${key}:
         ${pad}${hypr "movetoworkspace ${ws}"}
+    # wsbind'';
+
+    wsbindctrl = { key, ws, pad ? "                " }: /* yaml */ ''
+    # wsbindctrl
     ${pad}${mod}-ctrl-${key}:
         ${pad}${hypr "movetoworkspacesilent ${ws}"}
-    # wsbind'';
+    # wsbindctrl'';
 
     wsmon = { offset }: /* yaml */ ''
     # wsmon
                     ${wsbind { key = "r"; ws = "${offset}1"; }}
+                    ${wsbindctrl { key = "r"; ws = "${offset}1"; }}
+
                     ${wsbind { key = "s"; ws = "${offset}2"; }}
+                    ${wsbindctrl { key = "s"; ws = "${offset}2"; }}
+
                     ${wsbind { key = "t"; ws = "${offset}3"; }}
+                    ${wsbindctrl { key = "t"; ws = "${offset}3"; }}
+
                     ${wsbind { key = "h"; ws = "${offset}4"; }}
+                    ${mod}-esc:
+                        ${hypr "movetoworkspacesilent ${offset}4"}
 
                     ${wsbind { key = "n"; ws = "${offset}5"; }}
+                    ${mod}-left:
+                        ${hypr "movetoworkspacesilent ${offset}5"}
+
                     ${wsbind { key = "a"; ws = "${offset}6"; }}
+                    ${mod}-up:
+                        ${hypr "movetoworkspacesilent ${offset}6"}
+
                     ${wsbind { key = "i"; ws = "${offset}7"; }}
+                    ${mod}-down:
+                        ${hypr "movetoworkspacesilent ${offset}7"}
+
                     ${wsbind { key = "o"; ws = "${offset}8"; }}
+                    ${mod}-right:
+                        ${hypr "movetoworkspacesilent ${offset}8"}
     # wsmon'';
 in
 /* yaml */ ''
