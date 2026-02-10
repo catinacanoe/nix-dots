5dcb424ca832826c5e9baca5976029a4104b6f33
catinacanoe <catinacanoe@proton.me>
2026-02-09 20:27:40 -0800

    made fzf sidebar look nicer

--- home/fzf/default.nix
+++ home/fzf/default.nix
@@ -1,11 +1,17 @@
-{ config, ... }:
+{ lib, ... }:
 let
     c = (import ../../rice).col;
 in
 {
+    home.sessionVariables.FZF_DEFAULT_OPTS
+        = lib.mkForce "--scrollbar=' ' --pointer='>' --gutter=' ' --color bg:-1,bg+:-1,fg:${c.fg.h},fg+:${c.aqua.h},gutter:-1,header:${c.red.h},hl:${c.aqua.h},hl+:${c.green.h},info:${c.mg.h},marker:${c.blue.h},pointer:${c.blue.h},preview-bg:-1,preview-fg:${c.fg.h},prompt:${c.mg.h},spinner:${c.red.h}";
+
     programs.fzf = {
         enable = true;
         enableZshIntegration = true;
+        # defaultOptions = [
+        #     ''--scrollbar=" " --pointer=">" --gutter=" "''
+        # ];
         colors = {
             fg = c.fg.h;
             "fg+" = c.aqua.h;
